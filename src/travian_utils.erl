cd231a05e594755758919090c3ca7ae3cc68511e
SirWerto <sirwerto@gmail.com>
2023-10-30 20:41:50 +0100

    style(erlang formatter): add a plugin for erlang formatting

--- src/travian_utils.erl
+++ src/travian_utils.erl
@@ -2,64 +2,69 @@
 
 -export([distance401/4, distance/6, int_to_tribe/1, tribe_to_int/1]).
 
-
-
--spec distance401(X1 :: integer(), Y1 :: integer(), X2 :: integer(), Y2 :: integer()) -> float().
-distance401(X1, Y1, X2, Y2) when 
-      is_integer(X1),
-      is_integer(Y1),
-      is_integer(X2),
-      is_integer(Y2) ->
+-spec distance401(X1 :: integer(), Y1 :: integer(), X2 :: integer(), Y2 :: integer()) ->
+                     float().
+distance401(X1, Y1, X2, Y2)
+    when is_integer(X1), is_integer(Y1), is_integer(X2), is_integer(Y2) ->
     distance(401, 401, X1, Y1, X2, Y2).
 
--spec distance(Width :: integer(), Height :: integer(), X1 :: integer(), Y1 :: integer(), X2 :: integer(), Y2 :: integer()) -> float().
-distance(Width, Height, X1, Y1, X2, Y2) when
-      is_integer(Width),
-      Width > 0,
-      is_integer(Height),
-      Height > 0,
-      is_integer(X1),
-      is_integer(Y1),
-      is_integer(X2),
-      is_integer(Y2) ->
+-spec distance(Width :: integer(),
+               Height :: integer(),
+               X1 :: integer(),
+               Y1 :: integer(),
+               X2 :: integer(),
+               Y2 :: integer()) ->
+                  float().
+distance(Width, Height, X1, Y1, X2, Y2)
+    when is_integer(Width), Width > 0, is_integer(Height), Height > 0, is_integer(X1),
+         is_integer(Y1), is_integer(X2), is_integer(Y2) ->
     DiffX = abs(X1 - X2),
     DiffY = abs(Y1 - Y2),
-    math:pow(
-      math:pow(min(DiffX, Width - DiffX), 2) + math:pow(min(DiffY, Width - DiffY), 2),
-      0.5).
-
-
+    math:pow(math:pow(min(DiffX, Width - DiffX), 2) + math:pow(min(DiffY, Width - DiffY), 2),
+             0.5).
 
 -spec int_to_tribe(TribeEncoded :: integer()) -> {ok, atom()} | {error, unknown}.
 int_to_tribe(TribeEncoded) when is_integer(TribeEncoded) ->
     case TribeEncoded of
-	1 -> {ok, romans};
-	2 -> {ok, teutons};
-	3 -> {ok, gauls};
-	4 -> {ok, nature};
-	5 -> {ok, natars};
-	6 -> {ok, egyptians};
-	7 -> {ok, huns};
-	8 -> {ok, spartans};
-	_ -> {error, unknown}
+        1 ->
+            {ok, romans};
+        2 ->
+            {ok, teutons};
+        3 ->
+            {ok, gauls};
+        4 ->
+            {ok, nature};
+        5 ->
+            {ok, natars};
+        6 ->
+            {ok, egyptians};
+        7 ->
+            {ok, huns};
+        8 ->
+            {ok, spartans};
+        _ ->
+            {error, unknown}
     end.
 
 -spec tribe_to_int(Tribe :: atom()) -> {ok, integer()} | {error, unknown}.
 tribe_to_int(Tribe) when is_atom(Tribe) ->
     case Tribe of
-	romans -> {ok, 1};
-	teutons -> {ok, 1};
-	gauls -> {ok, 1};
-	nature -> {ok, 1};
-	natars -> {ok, 1};
-	egyptians -> {ok, 1};
-	huns -> {ok, 1};
-	spartans -> {ok, 1};
-	_ -> {error, unknown}
+        romans ->
+            {ok, 1};
+        teutons ->
+            {ok, 1};
+        gauls ->
+            {ok, 1};
+        nature ->
+            {ok, 1};
+        natars ->
+            {ok, 1};
+        egyptians ->
+            {ok, 1};
+        huns ->
+            {ok, 1};
+        spartans ->
+            {ok, 1};
+        _ ->
+            {error, unknown}
     end.
-    
-
-
-
-
-
--- src/travianmap.erl
+++ src/travianmap.erl
@@ -4,16 +4,13 @@
 
 -type travian_record() :: travianmap_mapline:travian_record().
 
-
-
 % @doc Parse a binary map and transform the results in to a list of tuples.
--spec parse_map(Binary_Map :: binary(), filter | no_filter) -> [{ok, travian_record()} | {error, any()}] | [travian_record()].
+-spec parse_map(Binary_Map :: binary(), filter | no_filter) ->
+                   [{ok, travian_record()} | {error, any()}] | [travian_record()].
 parse_map(Binary_Map, filter) when is_binary(Binary_Map) ->
     travianmap_map:parse_map(Binary_Map);
-
 parse_map(Binary_Map, no_filter) when is_binary(Binary_Map) ->
     travianmap_map:parse_map_nofilter(Binary_Map).
-
 
 % @doc Get all the current urls of the travian servers.
 -spec get_urls() -> {ok, [binary()]} | {error, any()}.
@@ -23,17 +20,18 @@ get_urls() ->
 % @doc Fetch the map of a current url.
 -spec get_map(Url :: binary()) -> {ok, binary()} | {error, any()}.
 get_map(Url) when is_binary(Url) ->
-    Options = [{body_format, binary},
-	      {full_result, false}],
+    Options = [{body_format, binary}, {full_result, false}],
     Endpoint = binary:bin_to_list(Url) ++ "/map.sql",
     case httpc:request(get, {Endpoint, []}, [], Options) of
-	{ok, {200, Body}} -> {ok, Body};
-	{ok, {StatusCode, _Body}} -> {error, {bad_status_code, StatusCode}};
-	{error, Reason} -> {error, Reason}
+        {ok, {200, Body}} ->
+            {ok, Body};
+        {ok, {StatusCode, _Body}} ->
+            {error, {bad_status_code, StatusCode}};
+        {error, Reason} ->
+            {error, Reason}
     end.
 
 % @doc Fetch the aditional info of a current url.
 -spec get_info(Url :: binary()) -> {ok, map()} | {error, any()}.
 get_info(Url) when is_binary(Url) ->
     travianmap_info:get_info(Url).
-
